baa8f9054e4df5095c6a9cc897c9eafd5d10ead6
Andre Vale Campos <acam201@mywhitecliffe.com>
2023-07-04 16:34:25 +1200

    Update

---
+++ Bank Account/Bank Account/CustomerCP.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Bank_Account
+{
+    partial class CustomerCP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(42, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(202, 379);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Customer";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(286, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 67);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Edit Customer";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(286, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 62);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Delete Customer";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // CustomerCP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "CustomerCP";
+            this.Text = "CustomerCP";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ Bank Account/Bank Account/Start.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bank_Account
+{
+    public partial class Start : Form
+    {
+        public static Start instance;
+        public Start()
+        {
+            InitializeComponent();
+            Start.instance = this;
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CustomerCP form = new CustomerCP();
+            form.Show();
+        }
+    }
+}
